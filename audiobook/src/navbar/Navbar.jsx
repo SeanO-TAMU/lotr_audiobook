95d1d1d402bd36750a7eeb8e2a50c685daac5ef5
Sean O'Connor <soc1641@icloud.com>
2025-02-07 11:01:01 -0600

    redid project start

---
+++ audiobook/src/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+
+function Navbar(){ // display map of world during the readings  (could later animate where they are on map?)
+     
+    return(
+        <div>
+            <h1>Audiobook</h1>
+            <h2>Home</h2>
+            <h2>Books</h2>
+        </div>
+    )
+}
+
+export default Navbar

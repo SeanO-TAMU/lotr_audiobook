d449366505a5515fc034c0569021b78d5291c92d
Sean O'Connor <soc1641@icloud.com>
2025-03-01 12:34:21 -0600

    some edits

--- audiobook/src/home/Home.jsx
+++ audiobook/src/home/Home.jsx
@@ -6,7 +6,7 @@ function Home (){
         <>
             <div className={styles.Title}>
                 <h1>The One Audiobook Library to Rule Them All</h1>
-                <p>A Journey Through Middle-Earth in Audiobook Form</p>
+                <Link to="/books"><p>A Journey Through Middle-Earth in Audiobook Form</p></Link>
             </div>
             <div className={styles.CTADiv}>
                 <div className={styles.CTA}>
@@ -19,7 +19,7 @@ function Home (){
                     <Link to="/books">Start Listening Now</Link>
                 </div>
                 <div className={styles.Bookimg}>
-                        <img src="/images/fellowship_cover.jpg"></img>
+                    <Link to="/books"><img src="/images/fellowship_cover.jpg"></img></Link>
                 </div>
                 <div className={styles.Bookinfo}>
                         <p className={styles.bookTitle}><strong>Title:</strong> The Lord of the Rings The Fellowship of the Ring</p>
@@ -32,28 +32,28 @@ function Home (){
             <div className={styles.Explore}>
                 <h2>Explore by Series</h2>
                 <div className={styles.ExploreBy}>
-                    <div className={styles.Book1}>
+                <Link to="/books" className={styles.NoLinkStyle1}><div className={styles.Book1}>
                         <h3>The Hobbit</h3>
                         <p>Tolkien’s classic tale of adventure, perfect for listeners of all ages. Begin with The Hobbit and explore the events that lead to The Lord of the Rings.</p>
-                    </div>
-                    <div className={styles.Book2}>
+                    </div></Link>
+                    <Link to="/books" className={styles.NoLinkStyle2}><div className={styles.Book2}>
                         <h3>The Lord of the Rings Trilogy</h3>
                         <p>The epic journey to destroy the One Ring, filled with heroes, battles, and the fate of Middle-Earth itself.</p>
-                    </div>
-                    <div className={styles.Book3}>
+                    </div></Link>
+                    <Link to="/books" className={styles.NoLinkStyle3}><div className={styles.Book3}>
                         <h3>The Silmarillion</h3>
                         <p>Explore the deep history of Middle-earth, from the creation of the world to the ancient tales of elves and men.</p>
-                    </div>
+                    </div></Link>
                 </div>
             </div>
             <div className={styles.OfTheDay}> 
                 <div className={styles.Quote}>
-                    <h2>Quote of the Day</h2>
+                    <h2>Quote oF The Day</h2>
                     <p>"There is some good in this world, and its worth fighting for."</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- J.R.R. Tolkien</p>
                 </div>
                 <div className={styles.Poem}>
-                    <h2>Poem of the Day</h2>
+                    <h2>Poem oF The Day</h2>
                     <p>All that is gold does not glitter, <br /> Not all those who wander are lost; <br /> The old that is strong does not wither, <br /> Deep roots are not reached by the frost. <br /> From the ashes a fire shall be woken, <br /> A light from the shadows shall spring; <br /> Renewed shall be blade that was broken, <br /> The crownless again shall be king.</p>
                 </div>
             </div>

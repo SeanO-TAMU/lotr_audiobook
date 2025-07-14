db209136f9ec268c590a36a91ab7c19e545ac25a
Sean O'Connor <soc1641@icloud.com>
2025-07-14 14:18:35 -0700

    moved audio element to its own file

--- audiobook/src/book/audiobook/AudiobookPage.jsx
+++ audiobook/src/book/audiobook/AudiobookPage.jsx
@@ -1,29 +1,7 @@
 import styles from "./AudiobookPage.module.css";
 import { useEffect, useState } from "react";
 import {titleText, chapText} from "../../helper.js";
-
-function ChapterPopup({ title, chapter, onClose }) {
-
-    let audioString = "http://localhost:5000/chapter?title=" + title + "&chapter=" + chapter;
-
-    // console.log("Title: ", title);
-    // console.log("Chapter: ", chapter);
-
-  return (
-    <div className={styles.popupOverlay}  onClick={onClose}>
-      <div className={styles.popupContent} onClick={(e) => e.stopPropagation()}>
-        
-        <p>{chapter}</p>
-        <audio controls>
-            <source src={audioString} type="audio/mpeg" />
-            Your browser does not support the audio element.
-        </audio>
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
-  );
-}
-
+import Audio from "./audio/Audio";
 
 function AudiobookPage({title}){
     const [chapterList, setchapterList] = useState([]);
@@ -66,7 +44,7 @@ function AudiobookPage({title}){
             </div>
 
             {showPopup && (
-                <ChapterPopup
+                <Audio
                 title={title}
                 chapter={popupChapter}
                 onClose={() => setshowPopup(false)}
---
+++ audiobook/src/book/audiobook/audio/Audio.jsx
@@ -0,0 +1,28 @@
+import styles from "./Audio.module.css";
+import {chapText} from "../../../helper.js";
+
+function Audio({ title, chapter, onClose }) {
+
+    let audioString = "http://localhost:5000/chapter?title=" + title + "&chapter=" + chapter;
+
+    // console.log("Title: ", title);
+    // console.log("Chapter: ", chapter);
+
+    let chapterString = chapText(chapter);
+
+  return (
+    <div className={styles.popupOverlay}  onClick={onClose}>
+      <div className={styles.popupContent} onClick={(e) => e.stopPropagation()}>
+        
+        <p>{chapText(chapter)}</p>
+        <audio controls className={styles.audio}>
+            <source src={audioString} type="audio/mpeg" />
+            Your browser does not support the audio element.
+        </audio>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+}
+
+export default Audio;

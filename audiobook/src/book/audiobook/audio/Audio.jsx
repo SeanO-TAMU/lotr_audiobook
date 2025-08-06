1a4c930091cd7600a859ef441a0066d6b00e3da0
Sean O'Connor <soc1641@icloud.com>
2025-08-06 16:37:32 -0700

    added fade in and out for images as well as the switch mechanic

--- audiobook/src/book/audiobook/audio/Audio.jsx
+++ audiobook/src/book/audiobook/audio/Audio.jsx
@@ -5,6 +5,10 @@ import {useState, useEffect} from "react";
 function Audio({ title, chapter, onClose }) {
 
     const [images, setImages] = useState([]);
+    const [imageNum, setImageNum] = useState(0);
+    const [folderNum, setFolderNum] = useState(0);
+    const [imageModulus, setImageModulus] = useState(0);
+    const [folderModulus, setFolderModulus] = useState(0);
 
     let audioString = "http://localhost:5000/chapter?title=" + encodeURIComponent(title) + "&chapter=" + encodeURIComponent(chapter);
 
@@ -18,9 +22,37 @@ function Audio({ title, chapter, onClose }) {
         })
     }, [title, chapter]);
 
+    useEffect(() => {
+
+      setFolderModulus(images.length);
+
+    }, [images]);
+
+    useEffect(() => {
+
+      setImageNum(0);
+
+    }, [images, folderNum]);
+
+    useEffect(() => {
+      
+      if (images.length > 0 && images[folderNum]) {
+        setImageModulus(images[folderNum].length);
+      }
+
+    }, [folderNum, images]);
+
     let imageString = `/library/${title}/chapters/${chapter}/images/`;
 
-    
+    useEffect(() => {
+      if (images.length > 0 && images[folderNum]?.length > 0) {
+        const intervalId = setInterval(() => {
+          setImageNum(prev => (prev + 1) % imageModulus); // replace this with a function that calls the fade and then switch?
+        }, 10000);
+
+        return () => clearInterval(intervalId); // Cleanup on unmount or dependency change
+      }
+    }, [imageModulus, folderNum, images]);
 
   return (
     <div className={styles.popupOverlay}  onClick={onClose}>
@@ -28,7 +60,9 @@ function Audio({ title, chapter, onClose }) {
         <img className={styles.closeIcon} src="/images/closeIcon.png" onClick={onClose}></img>
         <div className={styles.imgDiv}>
           {images.length === 0 && <img src="/images/lotr_logo.png"></img>}
-          {!(images.length === 0) && <img src={`${imageString}${images[0][0]}`}></img>}
+          {images.length > 0 && images[folderNum] && (
+            <img id="chapterImage" className={styles.chapImage} src={`${imageString}${images[folderNum][imageNum]}`} />
+          )}
         </div>
         
         <p>{chapText(chapter)}</p>

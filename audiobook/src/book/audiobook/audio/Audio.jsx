2cc90d64428d02985bc9ac4d1b4ac3406c68584a
Sean O'Connor <soc1641@icloud.com>
2025-08-05 22:25:25 -0700

    chapters now load images from the backend

--- audiobook/src/book/audiobook/audio/Audio.jsx
+++ audiobook/src/book/audiobook/audio/Audio.jsx
@@ -1,19 +1,34 @@
 import styles from "./Audio.module.css";
 import {chapText} from "../../../helper.js";
+import {useState, useEffect} from "react";
 
 function Audio({ title, chapter, onClose }) {
 
+    const [images, setImages] = useState([]);
+
     let audioString = "http://localhost:5000/chapter?title=" + encodeURIComponent(title) + "&chapter=" + encodeURIComponent(chapter);
 
-    // console.log("Title: ", title);
-    // console.log("Chapter: ", chapter);
+   useEffect(() => {
+        fetch(`http://localhost:5000/chapterImages?title=${encodeURIComponent(title)}&chapter=${encodeURIComponent(chapter)}`)
+        .then(res => res.json())
+        .then(data => {
+            setImages(data);
+            console.log("Fetched images: ", data);
+
+        })
+    }, [title, chapter]);
+
+    let imageString = `/library/${title}/chapters/${chapter}/images/`;
+
+    
 
   return (
     <div className={styles.popupOverlay}  onClick={onClose}>
       <div className={styles.popupContent} onClick={(e) => e.stopPropagation()}>
         <img className={styles.closeIcon} src="/images/closeIcon.png" onClick={onClose}></img>
         <div className={styles.imgDiv}>
-          <img src="/images/Balrog.jpg"></img>
+          {images.length === 0 && <img src="/images/lotr_logo.png"></img>}
+          {!(images.length === 0) && <img src={`${imageString}${images[0][0]}`}></img>}
         </div>
         
         <p>{chapText(chapter)}</p>
